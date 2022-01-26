e71db583ef874d85560f2e581a01c500e82a4a71
Lewis Skinner <97164619+LewisShipley@users.noreply.github.com>
2022-01-26 09:09:57 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+
+import tkinter
+
+window = tkinter.Tk()
+window.geometry("650x800")
+
+window.title("Decorator Tool")
+button_widget = tkinter.Button(window,text="Placeholder")
+button_widget.pack()
+
+window.mainloop()
+
